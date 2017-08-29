fcd2e289add41f230afedc48701f4dac002a9da6
AndreyIIT4 <andruha_kar@mail.ru>
2017-08-29 10:00:26 +0300

    Initial commit

---
+++ blelibrary/src/main/java/by/grsu/ftf/BleDevice.java
@@ -0,0 +1,16 @@
+package by.grsu.ftf;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BleDevice extends Service {
+    public BleDevice() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ blelibrary/src/main/java/by/grsu/ftf/KalmanFilter.java
@@ -0,0 +1,8 @@
+package by.grsu.ftf;
+
+/**
+ * Created by Lenovo on 29.08.2017.
+ */
+
+public class KalmanFilter {
+}
