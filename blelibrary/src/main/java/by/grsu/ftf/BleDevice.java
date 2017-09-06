c3a32de46d9b781a840fb96e598f5012d08a5c1a
AndreyIIT4 <andruha_kar@mail.ru>
2017-09-06 10:15:36 +0300

    Initial commit

--- blelibrary/src/main/java/by/grsu/ftf/BleDevice.java
+++ blelibrary/src/main/java/by/grsu/ftf/BleDevice.java
@@ -12,8 +12,6 @@ public class BleDevice extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
 
-
-
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
