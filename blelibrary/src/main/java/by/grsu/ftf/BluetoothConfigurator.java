f3416eb7b5b114b997cc6c915382513fbd8a4a37
AndreyIIT4 <andruha_kar@mail.ru>
2017-09-18 19:44:20 +0300

    Bluetooth

--- blelibrary/src/main/java/by/grsu/ftf/BluetoothConfigurator.java
+++ blelibrary/src/main/java/by/grsu/ftf/BluetoothConfigurator.java
@@ -1,8 +1,24 @@
 package by.grsu.ftf;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+
 /**
  * Created by Lenovo on 29.08.2017.
  */
 
-public class BluetoothConfigurator {
+public class BluetoothConfigurator extends Activity{
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    public void onBluetooth(){
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if (mBluetoothAdapter == null) {
+        // Device does not support Bluetooth
+    }
+    if (!mBluetoothAdapter.isEnabled()) {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
+}
 }
