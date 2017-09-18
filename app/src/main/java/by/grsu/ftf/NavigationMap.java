8aa6cfeea90ed52e0baa45c85d006113a4052adf
AndreyIIT4 <andruha_kar@mail.ru>
2017-09-18 19:33:42 +0300

    Initial commit

--- app/src/main/java/by/grsu/ftf/NavigationMap.java
+++ app/src/main/java/by/grsu/ftf/NavigationMap.java
@@ -1,8 +1,4 @@
 package by.grsu.ftf;
 
-/**
- * Created by Lenovo on 30.08.2017.
- */
-
 public class NavigationMap {
 }
--- blelibrary/src/main/java/by/grsu/ftf/BleDevice.java
+++ blelibrary/src/main/java/by/grsu/ftf/BleDevice.java
@@ -3,15 +3,16 @@ package by.grsu.ftf;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.annotation.IntDef;
 
 public class BleDevice extends Service {
-    public BleDevice() {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
